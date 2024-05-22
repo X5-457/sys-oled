b1744bd4ae7ddaeeea372deb11fc2d6bca11c814
X5-457 <165764495+X5-457@users.noreply.github.com>
2024-05-23 00:29:06 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,11 +14,11 @@ fi
 # apt-get update
 # apt-get install -y $DEPS
 
-echo "Installing luma.oled library"
-pip3 install --upgrade luma.oled
+# echo "Installing luma.oled library"
+# pip3 install --upgrade luma.oled
 
 echo "Installing sys-oled files"
-# cp -fv etc/sys-oled.conf  /etc
+cp -fv etc/sys-oled.conf  /etc
 cp -frv bin "$INSTALL_PATH"
 cp -frv share "$INSTALL_PATH"
 cp -frv system "$SYSTEMD_PATH"
